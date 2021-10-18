dd61cb2e9c8fbc4937437b8276886588954d2abb
tomrarcher <syvertcollege@gmail.com>
2021-10-18 17:58:06 +0200

    Make sure test report works

---
+++ src/test/java/HttpClientTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HttpClientTest {
+
+    @Test
+    void testForTests(){
+        assertTrue(true);
+    }
+
+}
+
+
