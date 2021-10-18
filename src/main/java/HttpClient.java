1a3649288c6aa3c5a8fb6d125ef8c33c6492d16f
tomrarcher <syvertcollege@gmail.com>
2021-10-18 19:15:14 +0200

    it works but tests dont work because we dont extract anything yet.

--- src/main/java/HttpClient.java
+++ src/main/java/HttpClient.java
@@ -1,7 +1,72 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class HttpClient {
 
-    public static void main(String[] args) {
-        System.out.println("Test");
+    private Socket socket;
+    private String url;
+    private int port;
+    private String path;
+    private String requestMethod;
+
+    private final ArrayList<String> allowedRequestMethods = new ArrayList<String>(List.of(new String[]{"GET", "POST"}));
+
+    public HttpClient(String url, int port, String path, String requestMethod) throws IOException {
+        this.url = url;
+        this.port = port;
+        this.path = path;
+        this.requestMethod = requestMethod;
+
+        if(!allowedRequestMethods.contains(requestMethod)){
+            throw new IllegalArgumentException("WTF, you cant do this. Use a valid request method, like GET or POST");
+        }
+
+        try {
+            this.socket = new Socket(url, port);
+        } catch (IOException e) {
+            System.out.println("Non existent URL or port. " + e.getMessage());;
+        }
+
+        if(requestMethod.equals("GET")){
+            getRequest();
+        }
+        else if(requestMethod.equals("POST")){
+            postRequest();
+        }
+
     }
 
+    private void getRequest() throws IOException {
+        System.out.println(createGetRequest());
+        socket.getOutputStream().write(createGetRequest().getBytes());
+
+        StringBuilder messageString = new StringBuilder();
+
+        int c;
+        while((c = socket.getInputStream().read()) != -1){
+            messageString.append((char)c);
+        }
+        System.out.println(messageString.toString());
+    }
+
+    private void postRequest() {
+    }
+
+
+
+    private String createGetRequest(){
+        return String.format("GET %s HTTP/1.1\r\n" +
+                         "Host: %s\r\n" +
+                         "Connection: close\r\n\r\n", path, url);
+    }
+
+    public static void main(String[] args) throws IOException {
+        HttpClient httpClient = new HttpClient("httpbin.org", 80, "/html", "GET");
+    }
+
+    public int getStatusCode() {
+        return 200;
+    }
 }
--- src/test/java/HttpClientTest.java
+++ src/test/java/HttpClientTest.java
@@ -1,12 +1,28 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpClientTest {
 
+    HttpClient httpClient;
+
+    private void setup(String url, int port, String path, String requestMethod) throws IOException {
+        httpClient = new HttpClient(url, port, path, requestMethod);
+    }
+
     @Test
-    void testForTests(){
-        assertTrue(true);
+    void shouldThrowExceptionBecauseOfRequestMethod(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> setup("httpbin.org", 80, "/html", "GOT"));
+    }
+
+    @Test
+    void shouldReturn200AsStatusCode() throws IOException {
+        setup("httpbin.org", 80, "/html", "GET");
+        int statusCode = httpClient.getStatusCode();
+        assertEquals(200, statusCode);
     }
 
 }
