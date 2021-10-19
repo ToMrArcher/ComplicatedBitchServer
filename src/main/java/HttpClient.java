33c5ae61781e2831ac1062beef80f443869b9393
tomrarcher <syvertcollege@gmail.com>
2021-10-19 18:08:24 +0200

    changed functionality of http client

--- src/main/java/HttpClient.java
+++ src/main/java/HttpClient.java
@@ -5,44 +5,15 @@ import java.util.List;
 
 public class HttpClient {
 
-    private Socket socket;
-    private String url;
-    private int port;
-    private String path;
-    private String requestMethod;
-
     private final ArrayList<String> allowedRequestMethods = new ArrayList<String>(List.of(new String[]{"GET", "POST"}));
 
-    public HttpClient(String url, int port, String path, String requestMethod) throws IOException {
-        this.url = url;
-        this.port = port;
-        this.path = path;
-        this.requestMethod = requestMethod;
+    HttpResponse httpResponse;
 
-        if(!allowedRequestMethods.contains(requestMethod)){
-            throw new IllegalArgumentException("WTF, you cant do this. Use a valid request method, like GET or POST");
-        }
+    public void get(String host, int port, String path) throws IOException {
+        Socket socket = new Socket(host, port);
+        socket.getOutputStream().write(createGetRequest(path, host).getBytes());
 
-        try {
-            this.socket = new Socket(url, port);
-        } catch (IOException e) {
-            System.out.println("Non existent URL or port. " + e.getMessage());;
-        }
-
-        if(requestMethod.equals("GET")){
-            getRequest();
-        }
-        else if(requestMethod.equals("POST")){
-            postRequest();
-        }
-
-    }
-
-    private void getRequest() throws IOException {
-        socket.getOutputStream().write(createGetRequest().getBytes());
-
-        HttpResponse response = new HttpResponse(socket);
-        System.out.println(response.getMessageBody());
+        httpResponse = new HttpResponse(socket);
     }
 
     private void postRequest() {
@@ -51,17 +22,22 @@ public class HttpClient {
 
 
 
-    private String createGetRequest(){
+    private String createGetRequest(String path, String url){
         return String.format("GET %s HTTP/1.1\r\n" +
                          "Host: %s\r\n" +
                          "Connection: close\r\n\r\n", path, url);
     }
 
-    public static void main(String[] args) throws IOException {
-        HttpClient httpClient = new HttpClient("httpbin.org", 80, "/html", "GET");
+    public HttpResponse response(){
+        return httpResponse;
     }
 
-    public int getStatusCode() {
-        return 200;
+    public static void main(String[] args) throws IOException {
+        HttpClient httpClient = new HttpClient();
+        httpClient.get("httpbin.org", 80, "/html");
+        System.out.println(httpClient.response().getMessageBody());
+        httpClient.get("httpbin.org", 80, "/");
+        System.out.println(httpClient.response().getMessageBody());
+
     }
 }
